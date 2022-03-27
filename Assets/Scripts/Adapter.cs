1b1aa10363108bd67eb264c31d18210593057368
Abuzyar Tazetdinov <tazetdinov9925@gmail.com>
2022-03-28 00:06:28 +0300

    + homework4

---
+++ Assets/Scripts/Adapter.cs
@@ -0,0 +1,12 @@
+﻿// Adapter allow us to use legacy code via new interface
+class Adapter : NewInterface {
+    private readonly LegacyInterface _legacy;
+
+    public Adapter(LegacyInterface legacy) {
+        _legacy = legacy;
+    }
+
+    public string DoSomething() {
+        return $"This is '{this._legacy.DoSomethingUsingLegacyInterface()}'";
+    }
+}
---
+++ Assets/Scripts/Legacy.cs
@@ -0,0 +1,12 @@
+﻿// Legacy interface
+
+public interface LegacyInterface {
+    public string DoSomethingUsingLegacyInterface();
+}
+
+// Implementation of legacy interface
+class Legacy : LegacyInterface {
+    public string DoSomethingUsingLegacyInterface() {
+        return "DoSomethingUsingLegacyInterface";
+    }
+}
